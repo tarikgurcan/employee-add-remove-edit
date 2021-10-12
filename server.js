c198568e8f2a989198c6e2c41bc065bd3ad395c6
tarikgurcan <tarikgurcan16@gmail.com>
2021-10-12 21:01:38 +0300

    heroku final 4

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
+console.log("hello")
 
 server.use(middlewares);
 server.use(router);
